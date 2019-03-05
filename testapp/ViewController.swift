91d8c206e0543bf1d98e903011fde64eb3ca4e82
Admin <admin@MAC10.local>
2019-03-05 12:08:53 +0700

    fixed bug

--- testapp/ViewController.swift
+++ testapp/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 import SQLite3
 class ViewController: UIViewController {
     
+
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var datePikerselect: UIDatePicker!
+    @IBOutlet weak var textFieldproduct: UITextField!
+    @IBOutlet weak var textFieldplace: UITextField!
+    
     let fileName = "db.sqlite"
     let fileManager = FileManager.default
     var dbPath = String()
@@ -18,87 +24,138 @@ class ViewController: UIViewController {
     var stmt: OpaquePointer?
     var pointer: OpaquePointer?
     var status : String = ""
-   
-   
-  
-    @IBAction func buttonSave(_ sender: UIButton) {
-        let place = textFieldplace.text!.trimmingCharacters(in: .whitespacesAndNewlines)
-        let product = textFieldproduct.text!.trimmingCharacters(in: .whitespacesAndNewlines)
-        
-        
-        self.sql = "INSERT INTO questionnaire (place,product,date,status) VALUES (?,?,?,'พอใจ')"
-        sqlite3_prepare (self.db , self.sql, -1 , &self.stmt , nil)
-       
-        sqlite3_bind_text(self.stmt , 1 , place, -1 , nil)
-        sqlite3_bind_text(self.stmt , 2 , product, -1 , nil)
-        
-        
-        sqlite3_step(self.stmt)
-        
-        textFieldplace.text  = ""
-        textFieldproduct.text = ""
-        select()
-        
-        print(" saved successfully")
-    }
-    @IBOutlet weak var textView: UITextView!
-    @IBOutlet weak var datePikerselect: UIDatePicker!
-    @IBOutlet weak var textFieldproduct: UITextField!
-    @IBOutlet weak var textFieldplace: UITextField!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         
         let dbURL = try! fileManager.url(
             for: .documentDirectory,
             in: .userDomainMask,
             appropriateFor : nil,
             create :false
-        )
-        .appendingPathComponent(fileName)
+            )
+            .appendingPathComponent(fileName)
         
         let openDb = sqlite3_open(dbURL.path,&db)
         if openDb != SQLITE_OK {
             print("Opeing database Error")
             return
         }
-        sql = "CREATE TABLE IF NOT EXISTS questionnaire" +
-              "id INTEGER PRIMARY KEY AUTOINCREMENT," +
-              "place TEXT, product TEXT , date TEXT , status TEXT"
+        sql = "CREATE TABLE IF NOT EXISTS Quest (id INTEGER PRIMARY KEY AUTOINCREMENT, product TEXT, place TEXT, date TEXT, feeling TEXT)"
+        
         let createTb = sqlite3_exec(db, sql, nil, nil, nil)
         if createTb != SQLITE_OK {
             let err = String(cString: sqlite3_errmsg(db))
             print(err)
         }
         
-        // Do any additional setup after loading the view, typically from a nib.
+        sql = "INSERT INTO Quest (id, product, place, date, feeling) VALUES ('1', 'สบู่อาบน้ำ' , 'โรบินสันปราจีนบุรี','22/02/2561','ดีมาก')"
+        sqlite3_exec(db,sql,nil,nil,nil)
+        
+        select()
+        
     }
+    
+    @IBAction func unhappy(_ sender: Any) {
+        let products = textFieldproduct.text! as NSString
+        let places = textFieldplace.text! as NSString
+        
+        let currentDate = datePikerselect.date
+        let myFormatter = DateFormatter()
+        myFormatter.dateFormat = "dd/MM/YYYY"
+        let thaiLocale = NSLocale(localeIdentifier: "TH_th")
+        myFormatter.locale = thaiLocale as Locale
+        let currentDateText = myFormatter.string(from: currentDate)
+        
+        self.sql = "INSERT INTO Quest VALUES (null,?,?,?,'ไม่พอใจ')";
+        sqlite3_prepare(self.db, self.sql, -1, &self.stmt, nil)
+        
+        sqlite3_bind_text(self.stmt, 1, products.utf8String, -1, nil)
+        sqlite3_bind_text(self.stmt, 2, places.utf8String , -1, nil)
+        sqlite3_bind_text(self.stmt, 3, currentDateText, -1, nil)
+        sqlite3_step(self.stmt)
+        
+        select()
+        
+    }
+    
+    @IBAction func nothing(_ sender: Any) {
+        let products = textFieldproduct.text! as NSString
+        let places = textFieldplace.text! as NSString
+        
+        let currentDate = datePikerselect.date
+        let myFormatter = DateFormatter()
+        myFormatter.dateFormat = "dd/MM/YYYY"
+        let thaiLocale = NSLocale(localeIdentifier: "TH_th")
+        myFormatter.locale = thaiLocale as Locale
+        let currentDateText = myFormatter.string(from: currentDate)
+        
+        self.sql = "INSERT INTO Quest VALUES (null,?,?,?,'เฉยๆ')";
+        sqlite3_prepare(self.db, self.sql, -1, &self.stmt, nil)
+        
+        sqlite3_bind_text(self.stmt, 1, products.utf8String, -1, nil)
+        sqlite3_bind_text(self.stmt, 2, places.utf8String , -1, nil)
+        sqlite3_bind_text(self.stmt, 3, currentDateText, -1, nil)
+        sqlite3_step(self.stmt)
+        
+        select()
+    }
+    
+    @IBAction func happy(_ sender: Any) {
+        
+        let products = textFieldproduct.text! as NSString
+        let places = textFieldplace.text! as NSString
+        
+        let currentDate = datePikerselect.date
+        let myFormatter = DateFormatter()
+        myFormatter.dateFormat = "dd/MM/YYYY"
+        let thaiLocale = NSLocale(localeIdentifier: "TH_th")
+        myFormatter.locale = thaiLocale as Locale
+        let currentDateText = myFormatter.string(from: currentDate)
+        
+        self.sql = "INSERT INTO Quest VALUES (null,?,?,?,'พอใจ')";
+        sqlite3_prepare(self.db, self.sql, -1, &self.stmt, nil)
+        
+        sqlite3_bind_text(self.stmt, 1, products.utf8String, -1, nil)
+        sqlite3_bind_text(self.stmt, 2, places.utf8String , -1, nil)
+        sqlite3_bind_text(self.stmt, 3, currentDateText, -1, nil)
+        sqlite3_step(self.stmt)
+        
+        select()
+        
+    }
+    
+    @IBAction func refreshComponent(_ sender: Any) {
+        self.viewDidLoad()
+    }
+    
     func select(){
-        sql = "SELECT * FROM questionnaire"
+        sql = "SELECT * FROM Quest"
         sqlite3_prepare(db , sql , -1, &pointer, nil)
         textView.text = ""
-        var id: Int32
-        var place: String
-        var product: String
-        var date: String
-        var status : String
         
-        while (sqlite3_step(pointer) == SQLITE_ROW){
+        var id: Int32
+        var product: String
+        var place: String
+        var date: String
+        var feeling : String
+        
+        while (sqlite3_step(pointer) == SQLITE_ROW) {
             id = sqlite3_column_int(pointer , 0)
             textView.text?.append("ลำดับที่ : \(id)\n")
+            
+            product = String(cString : sqlite3_column_text(pointer, 1))
+            textView.text?.append("สินค้า : \(product)\n")
+            
+            place = String(cString : sqlite3_column_text(pointer, 2))
+            textView.text?.append("สถาที่ : \(place)\n")
             
             date = String(cString : sqlite3_column_text(pointer, 3))
             textView.text?.append("วันที่ : \(date)\n")
             
-            place = String(cString : sqlite3_column_text(pointer, 1))
-            textView.text?.append("สถาที่ : \(place)\n")
-            
-            product = String(cString : sqlite3_column_text(pointer, 2))
-            textView.text?.append("สินค้า : \(product)\n")
-            
-            status = String(cString : sqlite3_column_text(pointer, 4))
-            textView.text?.append("ความพึงพอใจ : \(status)\n")
+            feeling = String(cString : sqlite3_column_text(pointer, 4))
+            textView.text?.append("ความพึงพอใจ : \(feeling)\n")
         }
     }
 
